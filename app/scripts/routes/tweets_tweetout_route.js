42fa3e402b862cbd7b6edb7b0d09c0bcd8bb5ab7
3v0k4 <riccardo.odone@gmail.com>
2014-05-23 11:07:21 -0400

    fixed componer msj title

--- app/scripts/routes/tweets_tweetout_route.js
+++ app/scripts/routes/tweets_tweetout_route.js
@@ -1,5 +1,5 @@
 Alarma.TweetsTweetoutRoute = Ember.Route.extend({
 	setupController: function(controller, model){
-		this.controllerFor('tweets').set('title', 'Componer Msj')
+		this.controllerFor('tweets').set('title', 'Componer Mensaje')
 	}
 });
