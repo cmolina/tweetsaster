6f3ab581560ca62192f72e9bbe4bae4c20e0df3a
Carlos Molina A <ing.cmolina@gmail.com>
2014-10-17 17:53:20 -0300

    'Tweet' template now is splitted in 3 sub-templates (index, map and pictures) Using Google Maps Embed for showing the position

--- app/scripts/models/tweet.js
+++ app/scripts/models/tweet.js
@@ -1,9 +1,19 @@
+Tweetsaster.GoogleApiKey = 'AIzaSyDiuj2l9-S2bE7K6i6Cxcgv6ZCDXaUiwYc';
+
 Tweetsaster.Tweet = DS.Model.extend({
   text: DS.attr('string'),
   created_at: DS.attr('string'),
   channel: DS.attr('string'),
+  user: DS.attr(),
+  // geo.coordinates[latitude, longitude]
+  geo: DS.attr(),
   formattedText: function() {
     var text = this.get('text');
     return text.charAt(0).toUpperCase() + text.slice(1);
-  }.property('text')
+  }.property('text'),
+  mapURL: function() {
+    var coords = this.get('geo').coordinates;
+    return 'https://www.google.com/maps/embed/v1/search?key=' + 
+      Tweetsaster.GoogleApiKey + '&q=' + coords[0] + ',' + coords[1];
+  }.property('geo'),
 });
