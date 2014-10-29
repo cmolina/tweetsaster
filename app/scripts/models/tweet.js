e413cc1ac24b5edba0f9b16cd74d14e21f818715
Carlos Molina A <ing.cmolina@gmail.com>
2014-10-29 01:46:14 -0300

    Using GeoJSON 'coordinates' attr in the 'Tweet' model

--- app/scripts/models/tweet.js
+++ app/scripts/models/tweet.js
@@ -5,13 +5,18 @@ Tweetsaster.Tweet = DS.Model.extend({
   created_at: DS.attr('string'),
   channel: DS.attr('string'),
   user: DS.attr(),
-  // geo.coordinates[latitude, longitude]
-  geo: DS.attr(),
+  // GeoJSON Point
+  coordinates: DS.attr(),
   // entities.media[{type: 'photo', media_url: 'http://...png'}]
   entities: DS.attr(),
+  in_reply_to_status_id: DS.attr('number'),
+
   pictures: function() {
     var mediaList = this.get('entities').media,
         picturesURL = [];
+    if (!mediaList)
+      return picturesURL;
+    
     mediaList.forEach(function(media) {
       if (media.type === 'photo')
         picturesURL.push(media.media_url);
@@ -23,9 +28,10 @@ Tweetsaster.Tweet = DS.Model.extend({
     var text = this.get('text');
     return text.charAt(0).toUpperCase() + text.slice(1);
   }.property('text'),
+  
   mapURL: function() {
-    var coords = this.get('geo').coordinates;
+    var coords = this.get('coordinates').coordinates;
     return 'https://www.google.com/maps/embed/v1/search?key=' + 
-      Tweetsaster.GoogleApiKey + '&q=' + coords[0] + ',' + coords[1];
-  }.property('geo'),
+      Tweetsaster.GoogleApiKey + '&q=' + coords[1] + ',' + coords[0];
+  }.property('coordinates'),
 });
