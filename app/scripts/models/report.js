01869b83a4a9570308132bb14437a68e3870cf61
Carlos Molina A <ing.cmolina@gmail.com>
2014-11-22 12:13:59 -0300

    The reports pictures are getted from the comments' pictures

--- app/scripts/models/report.js
+++ app/scripts/models/report.js
@@ -1,4 +1,13 @@
 Tweetsaster.Report = Tweetsaster.Tweet.extend({
   comments: DS.hasMany('comment', 
                        {async: true, inverse: 'inReplyToStatus'}),
+  allPictures: function() {
+    var allPictures = this.get('pictures').toArray();
+    this.get('comments').then(function(comments) {
+      comments.forEach(function(comment) {
+        allPictures.pushObjects(comment.get('pictures'));
+      });
+    }); 
+    return allPictures;
+  }.property('comments.length')
 });
