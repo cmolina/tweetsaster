fd45dcdc87079f106b0515f6142e92d82341957a
Carlos Molina A <ing.cmolina@gmail.com>
2014-12-04 20:20:03 -0300

    New `showToast` action https://github.com/kamranahmedse/jquery-toast-plugin

--- app/scripts/routes/application_route.js
+++ app/scripts/routes/application_route.js
@@ -1,5 +1,21 @@
 Tweetsaster.ApplicationRoute = Ember.Route.extend({
   model: function() {
     return this.store.find('report');
+  },
+  actions: {
+    showToast: function(text, afterHidden) {
+      // https://github.com/kamranahmedse/jquery-toast-plugin
+      $.toast().reset('all');
+      $.toast({
+        text: text,
+        allowToastClose: false,
+        hideAfter: 5000, // miliseconds
+        stack: false, // there should be only one toast at a time
+        position: 'bottom-center',        
+        bgColor: '#6f8ced',
+        textColor: '#ffffff',
+        afterHidden: afterHidden || function() {}
+      });
+    }
   }
 });
